29919f0fcc7fede33e6f1bc2dd8a8b00241cde56
bafredo <muyomafredrick@gmail.com>
2025-09-07 16:11:19 +0300

    Photos and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from './../assets/logo.svg'
 
 const Header = () => {
   return (
-    <div>
-        
+    <div className='w-full px-4 bg-[#703d92] h-[8vh] items-center'>
+        <div className='flex items-center h-[100%] gap-3'>
+            <h1>Home</h1>
+            <img src={logo} className='text-[]' />
+        </div>
     </div>
   )
 }
